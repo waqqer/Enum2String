138271f6a08ceb40104d6afe33820f9cff305f57
waq <147540375+waqqer@users.noreply.github.com>
2025-12-22 21:19:20 +0500

    Final

--- Enum2String.cs
+++ Enum2String.cs
@@ -56,3 +56,28 @@ public static class EnumExtension
         return attr is not null;
     }
 }
+
+/// <summary>
+/// Attribute that provides custom string representation for enum values.
+/// Apply to enum fields to specify alternative string names.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+public sealed class AsStringAttribute : Attribute
+{
+    /// <summary>
+    /// Gets the string representation of the enum value.
+    /// </summary>
+    public string name { get; }
+
+    /// <summary>
+    /// Initializes attribute with custom string name.
+    /// </summary>
+    /// <param name="name">Field string</param>
+    public AsStringAttribute(string name)
+           => this.name = name;
+    
+    /// <summary>
+    /// Initializes attribute with default name "none".
+    /// </summary>
+    public AsStringAttribute() : this("none") {}
+}
