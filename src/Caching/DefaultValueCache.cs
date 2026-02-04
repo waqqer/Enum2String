0456ab80e22b056caea00f9714adc09c822e1e64
waq <woqqa047@gmail.com>
2026-02-04 17:59:37 +0500

    Update DefaultValueCache.cs

--- src/Caching/DefaultValueCache.cs
+++ src/Caching/DefaultValueCache.cs
@@ -4,34 +4,33 @@ namespace Enum2String;
 
 internal static class DefaultValueCache
 {
-    public static ConcurrentDictionary<(Type, string), string> Cache { get; private set; } = [];
+    public static ConcurrentDictionary<Type, (string value, bool has)> Cache { get; private set; } = [];
 
-    public static void Add(Type type, string name, string value)
+    public static void Add(Type type, string value, bool has)
     {
-        Cache.TryAdd((type, name), value);
+        Cache.TryAdd(type, (value, has));
     }
 
-    public static bool TryAdd(Type type, string name, string value)
+    public static bool TryAdd(Type type, string value, bool has)
     {
-        return Cache.TryAdd((type, name), value);
+        return Cache.TryAdd(type, (value, has));
     }
 
-    public static string Get(Type type, string name)
+    public static (string value, bool has) Get(Type type)
     {
-        Cache.TryGetValue((type, name), out string? val);
-        return val is null ? name : val;
+        Cache.TryGetValue(type, out (string, bool) val);
+        return val;
     }
 
-    public static bool TryGet(Type type, string name, out string value)
+    public static bool TryGet(Type type, out (string value, bool has) value)
     {
-        string? val;
-        bool result = Cache.TryGetValue((type, name), out val);
-        value = val is null ? name : val;
+        bool result = Cache.TryGetValue(type, out (string, bool) val);
+        value = val;
         return result;
     }
 
-    public static bool Has(Type type, string name)
+    public static bool Has(Type type)
     {
-        return Cache.ContainsKey((type, name));
+        return Cache.ContainsKey(type);
     }
 }
