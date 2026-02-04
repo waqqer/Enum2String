0e36f83f9d060641dec1f1961ac1aa5f3152deaf
waq <woqqa047@gmail.com>
2026-02-04 20:49:08 +0500

    IValueRepresent interface xml docs

--- src/Interfaces/IValueRepresent.cs
+++ src/Interfaces/IValueRepresent.cs
@@ -1,6 +1,17 @@
 namespace Enum2String;
 
+/// <summary>
+/// Interface to group all attribute for enum value represent
+/// <br/><br/>
+/// Basic attributes:
+/// <br/>
+/// <see cref="DefaultStringValueAttribute"/> and
+/// <see cref="StringValueAttribute"/>
+/// </summary>
 public interface IValueRepresent
 {
+    /// <summary>
+    /// Enum string value
+    /// </summary>
     string Value { get; }
 }
